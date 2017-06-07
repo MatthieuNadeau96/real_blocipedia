d540f6e0af9f9808eb9bf9876ab93737797cfed6
Matthieu <matthieunadeau96@gmail.com>
2017-06-07 16:29:04 +0000

    Markdown Updates

--- app/helpers/wikis_helper.rb
+++ app/helpers/wikis_helper.rb
@@ -2,9 +2,4 @@ module WikisHelper
     def user_is_authorized_for_wiki?(wiki)
         current_user && (current_user == wiki.user || current_user.admin?)
     end
-    
-    def markdown(text)
-        extensions = [:tables, :fenced_code_blocks, :autolink, :strikethrough, :underline, :highlight, :quote, :footnotes]
-        Markdown.new(text, *extensions).to_html.html_safe
-    end
 end
